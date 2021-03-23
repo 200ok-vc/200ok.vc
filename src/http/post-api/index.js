54c22b57223baef852d8b6d0d04638f4e1892c82
Carter Rabasa <carter.rabasa@gmail.com>
2021-03-22 20:08:38 -0700

    build an API contact form

--- src/http/post-api/index.js
+++ src/http/post-api/index.js
@@ -1,15 +1,34 @@
 let arc = require('@architect/functions')
+let data = require('@begin/data')
+let validator = require('email-validator')
 
 let handler = async function (req) {
-  console.log(req)
-  return {
-    statusCode: 200,
-    json: {
-      echo: req.body.echo,
-      foo: 'bar',
-      baz: 'batttt'
+  let res;
+
+  // a valid HTTP request must include:
+  // - name | string
+  // - email | string
+  // - startup | string  
+  if (req.body.name && req.body.email && req.body.startup && validator.validate(req.body.email)) {
+    await data.set({ table: 'messages', key: req.body.email, ...req.body })
+    res = {
+      statusCode: 200,
+      json: {
+        result: 'success',
+        message: 'Thanks, we will be in touch 🤘'
+      }
     }
   }
+  else {
+    res = {
+      statusCode: 400,
+      json: {
+        result: 'error',
+        message: 'You must include all of the following POST parameters: name, email, body. Email must be valid.'
+      }
+    }
+  }
+  return res
 }
 
 exports.handler = arc.http.async(handler)
