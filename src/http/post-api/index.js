dedb23d26e2d65de831232b96e52647dd3440f0b
Carter Rabasa <carter.rabasa@gmail.com>
2021-04-02 15:38:12 -0700

    ping Slack channel when startup fills out form

--- src/http/post-api/index.js
+++ src/http/post-api/index.js
@@ -1,16 +1,27 @@
 let arc = require('@architect/functions')
 let data = require('@begin/data')
+let fetch = require('node-fetch')
 let validator = require('email-validator')
 
 let handler = async function (req) {
   let res;
-
-  // a valid HTTP request must include:
+  
+  // if "text" is passed as a parameter, then we are in localdev
+  if (req.body.text) {
+    console.log("A", req.body)
+    res = { body: "ok" }
+  }
+  // a valid staging/prod HTTP request must include:
   // - name | string
   // - email | string
-  // - startup | string  
-  if (req.body.name && req.body.email && req.body.startup && validator.validate(req.body.email)) {
+  // - startup | string
+  else if (req.body.name && req.body.email && req.body.startup && validator.validate(req.body.email)) {
+    // store a record in our DB
     await data.set({ table: 'messages', key: req.body.email, ...req.body })
+    // ping Slack
+    const params = new URLSearchParams()
+    params.append('text', `name: ${ req.body.name }\nemail: ${ req.body.email }\nstartup: ${ req.body.startup }`)
+    await fetch(process.env.SLACK_WEBHOOK_URL, {method: 'POST', body: params})
     res = {
       statusCode: 200,
       json: {
