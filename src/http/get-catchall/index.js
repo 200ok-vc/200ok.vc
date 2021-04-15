a85dc80433065b3da72fb1b5cab92f1f65eb8ae3
Carter Rabasa <carter.rabasa@gmail.com>
2021-04-14 19:58:28 -0700

    catchall router now supports routing to static files

--- src/http/get-catchall/index.js
+++ src/http/get-catchall/index.js
@@ -1,3 +1,4 @@
+let exists = fs.existsSync
 let arc = require('@architect/functions')
 let IndexView = require('@architect/views/index')
 let MemberView = require('@architect/views/member')
@@ -5,24 +6,52 @@ let PageView = require('@architect/views/pages')
 let NotFoundView = require('@architect/views/404')
 let memberData = require('@architect/shared/data/members.json')
 let skillData = require('@architect/shared/data/skills.json')
+let manifest = require('@architect/shared/static.json')
 
+// return the member record (if one exists) from memberData JSON
 function getMember(path) {
   return memberData.find((m) => m.slug === path.substring(1))
 }
 
+// return true if the markdown file exists, false otherwise
+function pageExists(path) {
+  let page = path.substr(1)
+  let doc = join(__dirname, '..', '..', 'views', 'content', `${ page }.md`)
+  return exists(doc)
+}
+
+// return truthy if the asset requested is in our static manifest JSON, falsy otherwise
+function staticExists(path) {
+  let asset = path.substr(1)
+  return manifest[asset]
+}
+
 /**
- * Index & member & page views
- * - This root function handles the main index view, and...
- * - Anything not specifically caught by explicitly defined paths (i.e. `GET /code-of-conduct`)
+ * This router passes the request to the appropriate view or static asset
  */
-async function Index (req) {
+async function Router (req) {
+  // root (/) request, return Index view
   if (req.rawPath === '/') {
       return await IndexView({ memberData, skillData })
   }
+  // the path matches the stub for a member (i.e. /carter-rabasa), return Member view
   else if (getMember(req.rawPath)) {
     return await MemberView({ member: getMember(req.rawPath) })
+  }
+  // the path matches a markdown file in our filesystem
+  else if (pageExists(req.rawPath)) {
+    return await PageView(req)
+  }
+  // the path matches a static file we know about
+  else if (staticExists(req.rawPath)) {
+    return {
+      statusCode: 301,
+      headers: {
+        location: arc.static(req.rawPath)
+      }
+    }
   }
   else return
 }
 
-exports.handler = arc.http.async(Index, PageView, NotFoundView)
+exports.handler = arc.http.async(Router, NotFoundView)
