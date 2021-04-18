6bb69f8b7d3a7fc165661d38a2502e00cdcc8682
Carter Rabasa <carter.rabasa@gmail.com>
2021-04-17 17:41:51 -0700

    debugging SG webhook request

--- src/http/post-sendgrid/index.js
+++ src/http/post-sendgrid/index.js
@@ -5,10 +5,10 @@ let handler = async function (req) {
   let res;
   // forward the contents of the incoming email
   sendgrid.setApiKey(process.env.SENDGRID_API_KEY)
-  console.log(req.body)
+  console.log(req)
   let msg = { ...req.body, from: process.env.FROM_EMAIL, to: process.env.FWD_EMAIL, reply_to: req.body.from }
   console.log(msg)
-  await sendgrid.send(msg)
+  //await sendgrid.send(msg)
   res = {
     statusCode: 200,
     body: "ok"
