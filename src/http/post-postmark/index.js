94c4c6059401b3e979fa0c113861fa0f837b66c3
Carter Rabasa <carter.rabasa@gmail.com>
2021-04-19 16:44:21 -0700

    added BOT_EMAIL

--- src/http/post-postmark/index.js
+++ src/http/post-postmark/index.js
@@ -3,7 +3,15 @@ let fetch = require('node-fetch')
 
 let handler = async function (req) {
   let { From, Subject, TextBody, HtmlBody } = req.body
-  let msg = { To: process.env.FWD_EMAIL, MessageStream: "outbound", From: process.env.FROM_EMAIL, ReplyTo: From, Subject, TextBody, HtmlBody }
+  // forward email to forwarding address
+  let To = process.env.FWD_EMAIL
+  // set reply-to to the original sender
+  let ReplyTo = From
+  // set from to the 200ok from address
+  From = process.env.BOT_EMAIL
+  // set message stream
+  let MessageStream = "outbound"
+  let msg = { To, MessageStream, From, ReplyTo, Subject, TextBody, HtmlBody }
   await fetch('https://api.postmarkapp.com/email', { method: 'POST', headers: { 'X-Postmark-Server-Token': process.env.POSTMARK_API_KEY, 'Content-Type': 'application/json', 'Accept': 'application/json' }, body: JSON.stringify(msg) })
   return {
     statusCode: 200,
