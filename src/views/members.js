5176aceca57a471f03b5d727d61731799ef595c4
Carter Rabasa <carter.rabasa@gmail.com>
2021-04-29 17:36:31 -0700

    added <title>

--- src/views/members.js
+++ src/views/members.js
@@ -3,6 +3,6 @@ let MembersComponent = require('./components/members')
 
 module.exports = async function MembersView({ members, skills, selectedSkills }) {
     let content = await MembersComponent({ members, skills, selectedSkills })
-    let html = Layout({ content, scripts: ['modules/entry/members.js'] })
+    let html = Layout({ title: 'Members', content, scripts: ['modules/entry/members.js'] })
     return { html }
 }
