ada68214a2feef58f6b778fc383296fda13bf0fb
Carter Rabasa <carter.rabasa@gmail.com>
2021-04-29 17:15:27 -0700

    do not overwrite global members value

--- src/shared/get-member-data.js
+++ src/shared/get-member-data.js
@@ -1,15 +1,17 @@
-let members = require('./data/members.json')
+const memberData = require('./data/members.json')
 
 module.exports = function GetMemberData (req) {
   // Collect all the speakers topics into an array with no duplicate topics
-  let skills = members.reduce((a, r) => [...new Set(a.concat(r.skills))].sort(), [])
+  let skills = memberData.reduce((a, r) => [...new Set(a.concat(r.skills))].sort(), [])
   let params = req.queryStringParameters || {}
   // Parse topics from the comma separated list in the query params
   let selectedSkills = params.skills && params.skills.split(',').map(t => t.trim())
+  // copy member data
+  let members = memberData.map(m => m)
   // If topics were passed as params
   if (selectedSkills) {
     // Only return speakers that have one of the selected topics
-    members = members.filter(s => {
+    members = memberData.filter(s => {
       let memberSkills = s.skills || []
       return memberSkills.some(memberSkill => selectedSkills.some(selectedSkill => memberSkill === selectedSkill))
     })
