ab8bd84b85362219f4a27fcf1d1b467fc67e6321
Carter Rabasa <carter.rabasa@gmail.com>
2021-04-29 17:02:28 -0700

    tweak json output

--- scripts/sync-airtable.js
+++ scripts/sync-airtable.js
@@ -23,17 +23,17 @@ async function fetchMembers({ skills }) {
     let fields = ['Full Name', 'Short Bio', 'Photo', 'Super Powers', 'Website']
     let json = await fetch(`${ baseUrl }/Members?view=Grid%20view${ fields.map((f) => '&fields%5B%5D='+encodeURIComponent(f)).join('') }`, { headers: {'Authorization': `Bearer ${ appKey }`}})
     let results = await json.json()
-    let members = results.records.map((r) => { return { 
+    let members = results.records.map((r) => { return {
         slug: slugify(r.fields['Full Name'], { lower: true }),
         full_name: r.fields['Full Name'],
         bio: r.fields['Short Bio'] ,
         photo: r.fields['Photo'][0].thumbnails.large.url,
-        super_powers: r.fields['Super Powers']
+        skills: r.fields['Super Powers']
             .map((id) => skills.find((s) => s.id === id)) // grab the skill objects that match the ids
             .map((s) => s.name), // grab the Name
         website: r.fields['Website']
     } })
-    return members;
+    return members
 }
 
 async function init() {
