e3278894f5a79d57eebecd0e08e18586cbcaa2a3
Carter Rabasa <carter.rabasa@gmail.com>
2021-08-24 09:18:43 -0700

    tweaked to accomodate changes in the Airtable

--- scripts/sync-airtable.js
+++ scripts/sync-airtable.js
@@ -16,25 +16,30 @@ async function fetchSkills() {
 
 async function fetchStartups() {
     let fields = ['Name', 'Portfolio', 'Logo', 'Website']
-    let json = await fetch(`${ baseUrl }/Startups?view=Grid%20view${ fields.map((f) => '&fields%5B%5D='+encodeURIComponent(f)).join('') }`, { headers: {'Authorization': `Bearer ${ appKey }`}})
+    let json = await fetch(`${ baseUrl }/Startups?view=viwaOHRkhTu9eaUXD${ fields.map((f) => '&fields%5B%5D='+encodeURIComponent(f)).join('') }`, { headers: {'Authorization': `Bearer ${ appKey }`}})
     let results = await json.json()
     return results.records.filter((r) => r.fields.Portfolio).map((r) => r.fields )
 }
 
 async function fetchMembers({ skills }) {
-    let fields = ['Full Name', 'Short Bio', 'Photo', 'Super Powers', 'Website']
-    let json = await fetch(`${ baseUrl }/Members?view=viwDU27YE88stIJeP${ fields.map((f) => '&fields%5B%5D='+encodeURIComponent(f)).join('') }`, { headers: {'Authorization': `Bearer ${ appKey }`}})
+    let fields = ['Full Name', 'Short Bio', 'Photo', 'Super Powers', 'Website', 'Public']
+    let json = await fetch(`${ baseUrl }/People?view=viwBNJyL8Mn2WcMtI${ fields.map((f) => '&fields%5B%5D='+encodeURIComponent(f)).join('') }`, { headers: {'Authorization': `Bearer ${ appKey }`}})
     let results = await json.json()
-    let members = results.records.map((r) => { return {
-        slug: slugify(r.fields['Full Name'], { lower: true }),
-        full_name: r.fields['Full Name'],
-        bio: r.fields['Short Bio'] ,
-        photo: r.fields['Photo'][0].thumbnails.large.url,
-        skills: r.fields['Super Powers']
-            .map((id) => skills.find((s) => s.id === id)) // grab the skill objects that match the ids
-            .map((s) => s.name), // grab the Name
-        website: r.fields['Website']
-    } })
+    console.log(results.records[0].fields)
+    let members = results.records
+        .filter((r) => {
+            return r.fields['Public'] === true
+        })
+        .map((r) => { return {
+            slug: slugify(r.fields['Full Name'], { lower: true }),
+            full_name: r.fields['Full Name'],
+            bio: r.fields['Short Bio'] ,
+            photo: r.fields['Photo'][0].thumbnails.large.url,
+            skills: r.fields['Super Powers']
+                .map((id) => skills.find((s) => s.id === id)) // grab the skill objects that match the ids
+                .map((s) => s.name), // grab the Name
+            website: r.fields['Website'] }})
+
     return members
 }
 
