bf97c8db62e19488d07d7101a1055f8950f9e1ae
bloodcarter <bloodcarter@gmail.com>
2021-09-02 12:56:21 +0400

    add prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,10 @@ module.exports = {
     node: true,
     es6: true
   },
-  extends: 'eslint:recommended',
+  extends: [
+    'eslint:recommended',
+    'prettier'
+  ],
   parserOptions: {
     ecmaVersion: 2018,
     sourceType: 'module'
